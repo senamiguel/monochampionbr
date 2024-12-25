3e2ecd74580a723f1c308ec408d56041464bbdfa
Miguel <miguelaugustosena@gmail.com>
2024-12-25 18:47:45 -0300

    adicionando consumo basico da api

--- src/app/components/create-profile/create-profile.component.ts
+++ src/app/components/create-profile/create-profile.component.ts
@@ -1,14 +1,23 @@
-import { NgIf,NgFor,NgSwitch, NgSwitchCase} from '@angular/common';
+import { NgIf, NgFor, NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ArrayService } from '../../services/array.service';
 import { ProfileComponent } from '../profile/profile.component';
 import { CommentComponent } from '../profile/comment/comment.component';
-import { AccountsComponent } from "../profile/accounts/accounts.component";
- 
+import { AccountsComponent } from '../profile/accounts/accounts.component';
+import { MonoApiService } from '../../services/monoapi.service';
+
 @Component({
   selector: 'app-create-profile',
-  imports: [NgIf, NgSwitch, NgSwitchCase, NgFor, ProfileComponent, CommentComponent, AccountsComponent],
+  imports: [
+    NgIf,
+    NgSwitch,
+    NgSwitchCase,
+    NgFor,
+    ProfileComponent,
+    CommentComponent,
+    AccountsComponent
+  ],
   templateUrl: './create-profile.component.html',
   styleUrl: './create-profile.component.css',
 })
@@ -19,29 +28,54 @@ export class CreateProfileComponent {
   username: string = '';
   selectedIconURL: string = '';
   selectediconIndex: number = 0;
-  iconBaseURL: string = 'https://ddragon-webp.lolmath.net/latest/img/profileicon/';
+  iconBaseURL: string =
+    'https://ddragon-webp.lolmath.net/latest/img/profileicon/';
+  iconID: number = 29;
   selectedChampionIndex: number = 0;
   selectedChampionName: string = '';
   availableChampionNames: string[];
   championIconMap: Map<string, string[]>;
- 
+
   constructor(
     private formBuilder: FormBuilder,
-    @Inject(ArrayService) private arrayService: ArrayService
+    @Inject(ArrayService) private arrayService: ArrayService,
+    private mono: MonoApiService
   ) {
- 
     this.profileForm = this.formBuilder.group({
       champions: [''],
     });
- 
     this.availableChampionNames = this.arrayService.champion_names;
     this.championIconMap = this.arrayService.champion_icons;
   }
- 
+  createAccount() {
+    if (!this.bioText || !this.username || !this.selectedChampionName || !this.iconID) {
+      console.error('Missing required fields');
+      return;
+    }
+    this.mono
+      .postCreateProfile({
+        nick: this.username.toLocaleLowerCase(),
+        bio: this.bioText,
+        champion: this.selectedChampionName,
+        iconID: this.iconID,
+        karma: 0,
+        elo: 'Mestre',
+        pdl: 1,
+      })
+      .subscribe({
+        next: () => {
+          console.log('Profile created');
+        },
+        error: (error) => {
+          console.error('Error creating profile', error);
+        }
+      });
+  }
+
   get championsControl() {
     return this.profileForm.get('champions');
   }
- 
+
   saveProfileDetails(value: string, fieldType: 'bio' | 'username'): void {
     if (fieldType === 'bio') {
       this.bioText = value;
@@ -49,44 +83,44 @@ export class CreateProfileComponent {
       this.username = value;
     }
   }
- 
+
   onChampionSelect(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     const selectedChampion = selectElement.value;
     const selectedIndex = selectElement.selectedIndex;
     this.selectedChampionIndex = selectedIndex;
- 
+
     const championIcons = this.championIconMap.get(selectedChampion);
- 
+
     if (championIcons && selectedIndex >= 0) {
       this.selectedChampionName = selectedChampion;
     } else {
       this.selectedIconURL = '';
     }
   }
- 
+
   generateIconURLForChampion(championName: string, iconIndex: number): string {
     const championIcons = this.championIconMap.get(championName);
- 
+
     if (championIcons && iconIndex >= 0 && iconIndex < championIcons.length) {
       return `${this.iconBaseURL}${championIcons[iconIndex]}.webp`;
     }
     return '';
   }
- 
+
   autoResizeTextarea(event: any): void {
     event.target.style.height = 'auto';
     event.target.style.height = `${event.target.scrollHeight}px`;
   }
- 
-  togglePage(page:number): void {
+
+  togglePage(page: number): void {
     this.currentPage = page;
   }
- 
+
   toggleIconSelection(iconIndex: number): void {
     const icons = document.querySelectorAll('[id^="icon"]');
     const championIcons = this.championIconMap.get(this.selectedChampionName);
- 
+
     icons.forEach((icon, index) => {
       if (index !== iconIndex) {
         icon.classList.remove('selected');
@@ -95,7 +129,10 @@ export class CreateProfileComponent {
     const selectedIcon = document.getElementById(`icon${iconIndex}`);
     if (selectedIcon && !selectedIcon.classList.contains('selected')) {
       selectedIcon.classList.add('selected');
-      this.selectedIconURL = `${this.iconBaseURL + championIcons![iconIndex]}.webp`;
+      this.iconID = Number(championIcons![iconIndex]);
+      this.selectedIconURL = `${
+        this.iconBaseURL + championIcons![iconIndex]
+      }.webp`;
     }
   }
 }
